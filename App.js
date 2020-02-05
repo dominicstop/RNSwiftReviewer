fc3bfacca403db0a2a72b052cb4fe5b35186c130
Dominic Go <dominicstop@gmail.com>
2020-02-06 01:15:48 +0800

    Initial navigation test

--- App.js
+++ App.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default class App extends Component {
+import createAnimatedSwitchNavigator from 'react-navigation-animated-switch';
+
+class AuthLoadingScreen extends Component {
+  static styles = StyleSheet.create({
+    rootContainer: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
   render() {
-    return (
-      <View>
+    const { styles } = AuthLoadingScreen;
 
+    return (
+      <View style={styles.rootContainer}>
+        <Text>AuthLoadingScreen</Text>
       </View>
     );
   };
 };
+
+class HomeScreen extends Component {
+  static styles = StyleSheet.create({
+    rootContainer: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  render() {
+    const { styles } = HomeScreen;
+
+    return (
+      <View style={styles.rootContainer}>
+        <Text>HomeScreen</Text>
+      </View>
+    );
+  };
+};
+
+const AppStack = createStackNavigator({ 
+  homeRoute: HomeScreen, 
+});
+
+const rootNavigator = createSwitchNavigator({
+    authRoute: AuthLoadingScreen,
+    appRoute : AppStack,
+  },{
+    initialRouteName: 'authRoute',
+  }
+);
+
+export default createAppContainer(rootNavigator);
