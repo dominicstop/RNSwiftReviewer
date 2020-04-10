4a8da4c84fa078fdd7fc76f81178a843c57263c7
Dominic Go <dominicstop@gmail.com>
2020-04-10 08:07:55 +0800

    ViewQuizModal wip
    added modalSection

--- src/components/ViewQuizSectionList.js
+++ src/components/ViewQuizSectionList.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { StyleSheet, SectionList, Text, View, TouchableOpacity, Dimensions } from 'react-native';
 
+import { ModalSection } from 'app/src/components/ModalSection';
+
 import * as Colors   from 'app/src/constants/Colors';
 import * as Helpers  from 'app/src/functions/helpers';
 
 export class ViewQuizSectionList extends React.PureComponent {
   render(){
-    return null;
+    return (
+      <ModalSection 
+        showBorderTop={false}
+        hasMarginBottom={false}
+      >
+        
+      </ModalSection>
+    );
   };
 };
--- src/components/ViewQuizSessionList.js
+++ src/components/ViewQuizSessionList.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { StyleSheet, SectionList, Text, View, TouchableOpacity, Dimensions } from 'react-native';
 
+import { ModalSection } from 'app/src/components/ModalSection';
+
+
 import * as Colors   from 'app/src/constants/Colors';
 import * as Helpers  from 'app/src/functions/helpers';
 
 export class ViewQuizSessionList extends React.PureComponent {
   render(){
-    return null;
+    return (
+      <ModalSection 
+        showBorderTop={false}
+        hasMarginBottom={false}
+      >
+
+      </ModalSection>
+    );
   };
 };
