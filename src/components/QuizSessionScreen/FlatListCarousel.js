8ae338dc2dbbcf681b21717d75b639aa649643cb
Dominic Go <dominicstop@gmail.com>
2020-04-15 01:27:45 +0800

    QuizSessionScreen: wip
    1) QuizSessionScreen: init. flatlist carousel impl. test.,
    2) Added FlatListCarousel: init. impl.,

---
+++ src/components/QuizSessionScreen/FlatListCarousel.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Dimensions, Clipboard } from 'react-native';
+
+import * as Helpers from 'app/src/functions/helpers';
+
+const { height: screenHeight, width: screenWidth } = Dimensions.get('screen');
+
+const ITEM_WIDTH  = screenWidth ;
+const ITEM_HEIGHT = screenHeight;
+
+export class FlatListCarousel extends React.PureComponent {
+
+  constructor(props){
+    super(props);
+
+    this.scrollY = 0;
+  };
+  
+  getProps(){
+    const { renderItem, ...props } = this.props;
+
+    return props;
+  };
+
+  _handleOnScrollEndDrag = async ({nativeEvent}) => {
+    const { onBeforeSnap } = this.props;
+    const { contentOffset } = nativeEvent;
+
+    onBeforeSnap && onBeforeSnap({
+      index: this.currentIndex,
+    });
+
+    this.scrollY = contentOffset.y;
+    this.flatlistRef.setNativeProps({scrollEnabled: false });
+    
+    await Helpers.timeout(250);
+    this.flatlistRef.setNativeProps({ scrollEnabled: true });
+  };
+
+  _handleOnScroll = ({nativeEvent}) => {
+    const { contentOffset } = nativeEvent;
+    this.scrollY = contentOffset.y;
+  };
+
+  _handleOnMomentumScrollEnd = ({nativeEvent}) => {
+    const { onSnap } = this.props;
+    const { contentOffset: {y} } = nativeEvent;
+
+    const scrollY      = ((y < 0)? 0 : y);
+    const currentIndex = Math.ceil(scrollY / ITEM_HEIGHT);
+
+    this.scrollY      = scrollY;
+    this.currentIndex = currentIndex;
+
+    console.log(`\n_handleOnMomentumScrollEnd: ${scrollY}`);
+    console.log(`currentIndex: ${onSnap}`);
+
+    onSnap && onSnap({
+      index: currentIndex
+    });
+  };
+
+  _handleGetItemLayout = (data, index) => ({
+    index,
+    length: ITEM_HEIGHT,
+    offset: ITEM_HEIGHT * index,
+  });
+
+  _renderItem = ({item, index}) => {
+    const { renderItem } = this.props;
+
+    return(
+      <View>
+        {renderItem && renderItem({item, index})}
+      </View>
+    );
+  };
+
+  render(){
+    const props = this.props;
+
+    return(
+      <FlatList
+        ref={r => this.flatlistRef = r}
+        style={styles.flatList}
+        renderItem={this._renderItem}
+        keyExtractor={item => item.key}
+        snapToAlignment={'center'}
+        decelerationRate={'fast'}
+        overScrollMode={'never'}
+        disableIntervalMomentum={true}
+        directionalLockEnabled={true}
+        disableScrollViewPanResponder={true}
+        scrollEventThrottle={500}
+        pagingEnabled={true}
+        onScrollEndDrag={this._handleOnScrollEndDrag}
+        onScroll={this._handleOnScroll}
+        onMomentumScrollEnd={this._handleOnMomentumScrollEnd}
+        {...props}
+      />
+    );
+  };
+};
+
+const styles = StyleSheet.create({
+
+});
--- src/screens/QuizSessionScreen.js
+++ src/screens/QuizSessionScreen.js
@@ -1,6 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Dimensions, Clipboard } from 'react-native';
 
+import { FlatListCarousel } from 'app/src/components/QuizSessionScreen/FlatListCarousel';
+
+import * as Helpers from 'app/src/functions/helpers';
+
+const DUMMY_DATA = [
+  { key: 1  },
+  { key: 2  },
+  { key: 3  },
+  { key: 4  },
+  { key: 5  },
+  { key: 6  },
+  { key: 7  },
+  { key: 8  },
+  { key: 9  },
+  { key: 10 },
+  { key: 11 },
+  { key: 12 },
+  { key: 13 },
+  { key: 14 },
+  { key: 15 },
+  { key: 16 },
+  { key: 17 },
+  { key: 18 },
+  { key: 19 },
+];
+
+const { height: screenHeight, width: screenWidth } = Dimensions.get('screen');
+
+const ITEM_HEIGHT = screenHeight;
+const ITEM_WIDTH  = screenWidth;
+
+class QuizQuestionItem extends React.PureComponent {
+  static styles = StyleSheet.create({
+    rootContainer: {
+      width: ITEM_WIDTH,
+      height: ITEM_HEIGHT,
+      backgroundColor: 'red',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+  });
+
+  render(){
+    const { styles } = QuizQuestionItem;
+    const { index } = this.props;
+
+    const rootContainerStyle = {
+      backgroundColor: ((index % 2 == 0)
+        ? 'red'
+        : 'green'
+      ),
+    };
+
+    return(
+      <View style={[styles.rootContainer, rootContainerStyle]}>
+        <Text style={{fontSize: 64}}>{index}</Text>
+      </View>
+    );
+  };
+};
 
 export class QuizSessionScreen extends React.Component {
   static navigationOptions = {
@@ -10,17 +70,54 @@ export class QuizSessionScreen extends React.Component {
   static styles = StyleSheet.create({
     rootContainer: {
       flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
+    },
+    flatList: {
+      flex: 1,
     },
   });
 
+  constructor(props){
+    super(props);
+
+    this.state = {
+      currentIndex: 0,
+    };
+  };
+
+  _handleSnap = ({index}) => {
+    this.setState({
+      currentIndex: index
+    });
+  };
+
+  _renderItem = ({item, index}) => {
+    return(
+      <QuizQuestionItem
+        {...{index}}
+      />
+    );
+  };
+
   render(){
     const { styles } = QuizSessionScreen;
+    const { currentIndex } = this.state;
 
     return(
       <View style={styles.rootContainer}>
-
+        <FlatListCarousel
+          data={DUMMY_DATA}
+          renderItem={this._renderItem}
+          onSnap={this._handleSnap}
+        />
+        <Text style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          fontSize: 30
+        }}>
+          {currentIndex}
+        </Text>
       </View>
     );
   };
