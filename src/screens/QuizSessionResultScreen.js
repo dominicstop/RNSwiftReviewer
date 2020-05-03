b26d096bf0de9f754e7455ef2681e012dff97fa1
Dominic Go <dominicstop@gmail.com>
2020-05-03 11:14:39 +0800

    QuizSessionResultScreen wip
    added placeholder text results

--- src/screens/QuizSessionResultScreen.js
+++ src/screens/QuizSessionResultScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
 
 import { ListCard } from 'app/src/components/ListCard';
 import { HeaderValues } from 'app/src/constants/HeaderValues';
+import { SNPQuizSessionResult } from '../constants/NavParams';
+import { QuizSessionKeys, QuizSessionScoreKeys } from '../constants/PropKeys';
 
 
 const HEADER_HEIGHT = HeaderValues.getHeaderHeight(false);
+
+
 
 
 class SessionResults extends React.Component {
@@ -13,12 +17,33 @@ class SessionResults extends React.Component {
 
   });
 
+  constructor(props){
+    super(props);
+  };
+
   render(){
     const { styles } = SessionResults;
+    const { session } = this.props;
+
+    const scores = session[QuizSessionKeys.sessionScore];
+
+    const scoreWrong             = scores[QuizSessionScoreKeys.scoreWrong];
+    const scoreCorrect           = scores[QuizSessionScoreKeys.scoreCorrect];
+    const scoreIncorrect         = scores[QuizSessionScoreKeys.scoreIncorrect];
+    const scoreTotalItems        = scores[QuizSessionScoreKeys.scoreTotalItems];
+    const scorePercentCorrect    = scores[QuizSessionScoreKeys.scorePercentCorrect];
+    const scorePercentUnanswered = scores[QuizSessionScoreKeys.scorePercentUnanswered];
+    
 
     return (
       <ListCard>
-
+        <Text>{'WORK IN PROGRESS'}</Text>
+        <Text>{`scoreWrong: ${scoreWrong}`}</Text>
+        <Text>{`scoreCorrect: ${scoreCorrect}`}</Text>
+        <Text>{`scoreIncorrect: ${scoreIncorrect}`}</Text>
+        <Text>{`scoreTotalItems: ${scoreTotalItems}`}</Text>
+        <Text>{`scorePercentCorrect: ${scorePercentCorrect}`}</Text>
+        <Text>{`scorePercentUnanswered: ${scorePercentUnanswered}`}</Text>
       </ListCard>
     );
   };
@@ -30,11 +55,21 @@ export class QuizSessionResultScreen extends React.Component {
   };
 
   render(){
+    const { navigation } = this.props;
+    const { params } = navigation.state;
+
+    const quiz = params[SNPQuizSessionResult.quiz];
+    const session = params[SNPQuizSessionResult.session];
+    const sessions = params[SNPQuizSessionResult.sessions];
+
+
     return(
       <ScrollView
         contentInset={{ top: HEADER_HEIGHT }}
       >
-        <SessionResults/>
+        <SessionResults
+          {...{session}}
+        />
       </ScrollView>
     );
   };
